9b0919b09f45912afc9183c6ce5b14d3b45ec297
Ralph Howard Azarcon <103555409+Yukwiczyczy@users.noreply.github.com>
2024-09-13 17:58:39 +0800

    Create main.py

---
+++ turtle-dot/main.py
@@ -0,0 +1,53 @@
+from turtle import Turtle, Screen
+import random
+
+howard_turtle = Turtle()
+
+# Change a new shape and color
+howard_turtle.shape('turtle')
+howard_turtle.color('green')
+
+# Turtle movement to draw square
+def create_square():
+    
+    for i in range(4):
+        howard_turtle.forward(100)
+        howard_turtle.right(90)
+
+
+def create_dashline():
+    howard_turtle.penup()
+    howard_turtle.setx(-500)
+    
+    for i in range(13):
+        howard_turtle.pendown()
+        howard_turtle.forward(20)
+        howard_turtle.penup()
+        howard_turtle.forward(20)
+
+
+def create_complex_shapes():
+    max_sides = 8
+    
+    starting_count = 3
+    for draw in range(max_sides):
+        angle_turn = int(360/starting_count)
+        
+        # random_number = random.randint(0, 256)
+        random_color = tuple (random.random() for _ in range(3))
+        print(random_color)
+        x,y,z = random_color
+        howard_turtle.color(z,y,z)
+        
+        for _ in range(starting_count):
+            howard_turtle.forward(100)
+            howard_turtle.right(angle_turn)
+            
+        starting_count += 1
+    
+create_complex_shapes()
+
+
+# show screen
+screen = Screen()
+screen.exitonclick()
